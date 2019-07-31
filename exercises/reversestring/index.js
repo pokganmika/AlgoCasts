7c740a07e8c90ff092d47fc507cce57f3efd9835
pokganmika <calmgust@gmail.com>
2019-08-01 00:24:16 +0900

    Complete 190801 reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 // --- Directions
 // Given a string, return a new string with the reversed
 // order of characters
@@ -6,6 +7,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // my solution
+
+  // ===== for loop =====
+  // let result = '';
+  // for (let i = 0; i < str.length; i++) { 
+  //   result = str[i] + result;
+  // }
+  // return result;
+
+  // ===== reduce =====
+  // return str.split('').reverse().reduce((prev, curr) => prev + curr);
+  // return str.split('').reduce((prev, curr) => curr + prev);
+
+  // ----- -----
+
+  // solution #1 (method)
+  // return str.split('').reverse().join('');
+
+  // solution #2 (for of loop)
+  // let reversed = '';
+  // for (let character of str) { 
+  //   reversed = character + reversed;
+  // }
+  // return reversed;
+
+  // solution #3
+  // return str.split('').reduce((reversed, character) => {
+  //   return character + reversed;
+  // }, '');
+
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
