1d3611fa79b1065d5a15869b2b431f6bad4d60a2
pokganmika <calmgust@gmail.com>
2019-08-06 00:48:02 +0900

    Complete 190806 palidrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 // --- Directions
 // Given a string, return true if the string is a palindrome
 // or false if it is not.  Palindromes are strings that
@@ -7,6 +8,27 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  // my solution
+
+  // const basicArr = str.split('');
+  // const reversedArr = str.split('').reverse();
+
+  // for (let i = 0; i < str.length; i++) { 
+  //   if (basicArr[i] !== reversedArr[i]) return false;
+  // }
+  // return true;
+
+  // ----- -----
+
+  // solution #1 (method)
+  // const reversed = str.split('').reverse().join('');
+  // return reversed === str;
+
+  // solution #2 (every) *****
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
+}
 
 module.exports = palindrome;
