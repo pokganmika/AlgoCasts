54e22dcb55367e9fbf9b0b4958206d4a5cb2ac10
pokganmika <calmgust@gmail.com>
2019-08-13 23:34:02 +0900

    Complete 190813 capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -1,3 +1,4 @@
+/* jshint esversion: 6 */
 // --- Directions
 // Write a function that accepts a string.  The function should
 // capitalize the first letter of each word in the string then
@@ -7,6 +8,35 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  // my solution
+  // let result = str.split('');
+  // for (let i = 0; i < result.length; i++) { 
+  //   if (result[0]) result[0] = result[0].toUpperCase();
+  //   if (result[i] === ' ' && result[i + 1] !== undefined) result[i + 1] = result[i + 1].toUpperCase();
+  // }
+  // return result.join('');
+
+  // ===== slice =====
+
+  // const words = [];
+
+  // for (let word of str.split(' ')) { 
+  //   words.push(word[0].toUpperCase() + word.slice(1));
+  // }
+
+  // return words.join(' ');
+
+  // ===== for loop =====
+
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) { 
+    if (str[i - 1] === ' ') result += str[i].toUpperCase();
+    else result += str[i];
+  }
+
+  return result;
+}
 
 module.exports = capitalize;
