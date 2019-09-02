a2316df8c9c6de6e5a1aa6a9f40fbce577ddeee9
pokganmika <calmgust@gmail.com>
2019-09-03 01:51:38 +0900

    Complete 190903 steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -1,3 +1,4 @@
+/* jshint esversion: 6 */
 // --- Directions
 // Write a function that accepts a positive number N.
 // The function should console log a step shape
@@ -17,6 +18,47 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// #1
+// function steps(n) {
+//   for (let row = 0; row < n; row++) { 
+//     let stair = '';
+
+//     for (let column = 0; column < n; column++) { 
+//       if (column <= row) {
+//         stair += '#';
+//       } else { 
+//         stair += ' ';
+//       }
+//     }
+
+//     console.log(stair);
+//   }
+// }
+
+// #2
+function steps(n, row = 0, stair = '') { 
+  if (n === row) { 
+    return;
+  }
+
+  if (n === stair.length) { 
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  // if (stair.length <= row) {
+  //   stair += '#';
+  // } else { 
+  //   stair += ' ';
+  // }
+  // steps(n, row, stair);
+
+  const add = stair.length <= row ? '#' : ' ';
+  steps(n, row, stair + add);
+  
+}
+
+console.log('result : ', steps(3));
+console.log('end');
 
 module.exports = steps;
