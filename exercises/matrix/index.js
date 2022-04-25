1858fc24996fdeb4b4b47e16fdac4f9980508568
pokganmika <calmgust@gmail.com>
2022-04-25 23:43:38 +0900

    Complete 220425_matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,87 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const result = Array.from( Array( n ), () => Array(n) );
+
+  let count = 1;
+
+  let startColumn = 0,
+    startRow = 0,
+    endColumn = n - 1,
+    endRow = n - 1;
+
+  while ( count <= n * n ) {
+    for ( let i = startColumn; i <= endColumn; i++ ) {
+      result[startRow][i] = count;
+      count++;
+    } 
+    startRow++;
+    for ( let i = startRow; i <= endRow; i++ ) {
+      result[i][endColumn] = count;
+      count++;
+    }
+    endColumn--;
+    for ( let i = endColumn; i >= startColumn; i-- ) {
+      result[endRow][i] = count;
+      count++;
+    }
+    endRow--;
+    for ( let i = endRow; i >= startRow; i-- ) {
+      result[i][startColumn] = count;
+      count++;
+    } 
+    startColumn++;
+  }
+
+  return result;
+}
+
+// SOLUTION
+// function matrix(n) {
+//   const result = [];
+
+//   for ( let i = 0; i < n; i++ ) {
+//     result.push( [] );
+//   }
+
+//   let counter = 1;
+//   let startColumn = 0;
+//   let endColumn = n - 1;
+//   let startRow = 0;
+//   let endRow = n - 1;
+
+//   while ( startColumn <= endColumn && startRow <= endRow ) {
+//     // Top row
+//     for ( let i = startColumn; i <= endColumn; i++ ) {
+//       result[startRow][i] = counter;
+//       counter++;
+//     }
+//     startRow++;
+
+//     // Right column
+//     for ( let i = startRow; i <= endRow; i++ ) {
+//       result[i][endColumn] = counter;
+//       counter++;
+//     }
+//     endColumn--;
+
+//     // Bottom row
+//     for ( let i = endColumn; i >= startColumn; i-- ) {
+//       result[endRow][i] = counter;
+//       counter++;
+//     }
+//     endRow--;
+
+//     // Start column
+//     for ( let i = endRow; i >= startRow; i-- ) {
+//       result[i][startColumn] = counter;
+//       counter++;
+//     }
+//     startColumn++;
+//   }
+
+//   return result;
+// }
 
 module.exports = matrix;
